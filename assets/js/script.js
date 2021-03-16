4b1cd2b91e7e4d67177d0770c2b63401d8c5af1c
Brian Adams <bpeteradams@gmail.com>
2021-03-16 14:01:10 -0400

    commented out first api call function

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,30 +14,30 @@ function constructURL1() {
 
 //construct fetch
 
-function queryAPI() {
-   let requestURL = constructURL1();
-    fetch(requestURL)
-        .then((response)=> {
-            console.log(response)
-            return response.json();
-        })
-        .then((data)=> {
-            console.log(data);
-            var coord = data.coord
-            return coord;
+// function queryAPI() {
+//    let requestURL = constructURL1();
+//     fetch(requestURL)
+//         .then((response)=> {
+//             console.log(response)
+//             return response.json();
+//         })
+//         .then((data)=> {
+//             console.log(data);
+//             var coord = data.coord
+//             return coord;
             
         
-        })
-        .catch((err)=>{
-            console.error(err);
-        })
+//         })
+//         .catch((err)=>{
+//             console.error(err);
+//         })
     
-}
+// }
 
 async function queryAPI2() {
     let response1 = await fetch(constructURL1());
-    let data = await response1.json()
-    let coord = await data.coord;
+    let data1 = await response1.json()
+    let coord = await data1.coord;
     fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${coord.lat}&lon=${coord.lon}&exclude=minutely,hourly,alerts&units=imperial&appid=${defaultAPIKey}`)
         .then((response2)=>{
             console.log(response2);
@@ -46,8 +46,9 @@ async function queryAPI2() {
         .then((data)=>{
             console.log(data.current);
             console.log(data.daily);
+            console.log(data.daily[0].dt)
         })
         .catch((err)=> {
-            console.err(err);
+            console.error(err);
         })
     };
