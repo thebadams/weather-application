093a5bea8dfaceb9d0b1fd6812bdf401a5a3338b
Brian Adams <bpeteradams@gmail.com>
2021-03-16 12:57:23 -0400

    wrote a second api call, parsing the coordinates from the first api call using async/await

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ var cityName = document.querySelector("#city-name");
 
 //construct requestURL #2
 //https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-function constructURL() {
+function constructURL1() {
     var cityQuery = cityName.value;
     var requestURL = `http://api.openweathermap.org/data/2.5/weather?q=${cityQuery}&appid=${defaultAPIKey}`
     return requestURL;
@@ -15,7 +15,7 @@ function constructURL() {
 //construct fetch
 
 function queryAPI() {
-   let requestURL = constructURL();
+   let requestURL = constructURL1();
     fetch(requestURL)
         .then((response)=> {
             console.log(response)
@@ -33,3 +33,20 @@ function queryAPI() {
         })
     
 }
+
+async function queryAPI2() {
+    let response1 = await fetch(constructURL1());
+    let data = await response1.json()
+    let coord = await data.coord;
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${coord.lat}&lon=${coord.lon}&appid=${defaultAPIKey}`)
+        .then((response)=>{
+            console.log(response);
+            return response.json();
+        })
+        .then((data)=>{
+            console.log(data);
+        })
+        .catch((err)=> {
+            console.err(err);
+        })
+    };
